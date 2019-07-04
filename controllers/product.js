9cc7a64f7ca7d02c28d19f2c8df72ec1e0b8f43f
pprachit09 <pprachit09@gmail.com>
2019-07-04 22:43:52 +0530

    CRUD operations on Products

--- controllers/product.js
+++ controllers/product.js
@@ -47,3 +47,82 @@ exports.create = (req, res) => {
         })
     })
 }
+
+//to get the product details
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec( (err, product) => {
+        if(err || !product){
+            return res.status(404).json({
+                error: "Product not found"
+            })
+        }
+        req.product = product
+        next()
+    })
+}
+
+//read the product
+exports.read = (req, res) => {
+    req.product.photo = undefined
+    return res.json(req.product)
+}
+
+//delete the product
+exports.remove = (req, res) => {
+    let product = req.product
+    product.remove( err => {
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.json({
+            message: "Deleted"
+        })
+    })
+}
+
+//update the product
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse( req, (err, fields, files) => {
+        if(err){
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+        
+        //validation for fields
+        const { name, description, price, category, shipping, quantity } = fields
+
+        if(!name || !description || !price || !category || !shipping || !quantity){
+            return res.status(400).json({
+                error: 'All fields are required'
+            })
+        }
+
+        let newProduct = req.product
+        newProduct = _.extend(newProduct, fields)
+
+        //to save photo
+        if(files.photo){
+            if(files.photo.size > 100000){
+                return res.status(400).json({
+                    error: 'Image size should be less than 1 MB'
+                })
+            }
+            newProduct.photo.data = fs.readFileSync(files.photo.path)
+            newProduct.photo.contentType = files.photo.type 
+        }
+
+        newProduct.save( (err, data) => {
+            if(err || !data){
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(data)
+        })
+    })
+}
